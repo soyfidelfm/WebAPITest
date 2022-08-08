2040d0f38333f764935fe4a691ffea6bcf20be77
soyfidelfm <fidel_filio@hotmail.com>
2022-08-08 15:28:35 -0700

    Adding new controller that receive 3 parameters

---
+++ WebAPITest/Controllers/ListifyController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPITest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ListifyController : ControllerBase
+    {        
+        [HttpGet(Name = "listify")]
+        public int listify(int begin, int end, int index)
+        {
+            return Enumerable.Range(begin, end).ElementAt(index);
+        }
+    }
+}
